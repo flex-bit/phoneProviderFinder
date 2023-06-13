1e3c620b3f1128f38ddbf4037879c0539a5e27a4
John Infante <junio212132@gmail.com>
2023-06-13 00:42:34 -0400

    finished with the packaga

--- phoneProvider/phoneProvider.go
+++ phoneProvider/phoneProvider.go
@@ -1,12 +1,17 @@
 package phoneProvider
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"regexp"
 )
+
+type phoneUsNumber struct {
+	AreaCode string `json:"arecode"`
+	Prefix   string `json:"prefix"`
+	Local    string `json:"local"`
+}
 
 type PhoneInfo struct {
 	Provider string `json:"provider"`
@@ -15,12 +20,25 @@ type PhoneInfo struct {
 	Type     string `json:"type"`
 }
 
-func PhoneProviderGet() {
-	apiUrl := "https://www.fonefinder.net/findome.php?npa=614&nxx=364&thoublock=1215"
+func PhoneProviderGet(number string) PhoneInfo {
+	//replace any character that is not a number
+	patternDeleteCharacterNonDigit := regexp.MustCompile("[^\\d]")
+	cleanNumber := patternDeleteCharacterNonDigit.ReplaceAllString(number, "")
+
+	//check if phone number is 10 digits long if not terminate
+	if len(cleanNumber) != 10 {
+		log.Fatalln("\nYour phone number should be 10 digit long please try again")
+	}
+	// Extract phone number into 3 peaces, AreaCode,Prefix Local
+	patternExtractThreePeacesPhoneNumber := regexp.MustCompile("(\\d{0,3})(\\d{0,3})(\\d{0,4})")
+	providePhoneNumberThreePeaces := patternExtractThreePeacesPhoneNumber.FindStringSubmatch(cleanNumber)
+	currentPhoneNumber := phoneUsNumber{AreaCode: providePhoneNumberThreePeaces[1], Prefix: providePhoneNumberThreePeaces[2], Local: providePhoneNumberThreePeaces[3]}
+	apiUrl := "https://www.fonefinder.net/findome.php?npa=" + currentPhoneNumber.AreaCode + "&nxx=" + currentPhoneNumber.Prefix + "&thoublock=" + currentPhoneNumber.Local
 	res, err := http.Get(apiUrl)
 	if err != nil {
 		log.Fatalln(err)
 	}
+
 	body, err := io.ReadAll(res.Body)
 	defer res.Body.Close()
 
@@ -31,5 +49,5 @@ func PhoneProviderGet() {
 	pattern := regexp.MustCompile("findcity\\.php\\?.*?cityname=(\\w+)&state=(\\w+)'>\\w+.*?(\\w+)<\\/A.*?(\\w+).php'>(.*?)<\\/A><TD>(.*?)<")
 	matchPhoneInfo := pattern.FindStringSubmatch(string(body))
 	phoneinfo := PhoneInfo{Provider: matchPhoneInfo[4], State: matchPhoneInfo[3], City: matchPhoneInfo[1], Type: matchPhoneInfo[6]}
-	fmt.Printf("State: %s, City: %s, Provider: %s", phoneinfo.State, phoneinfo.City, phoneinfo.Provider)
+	return phoneinfo
 }
